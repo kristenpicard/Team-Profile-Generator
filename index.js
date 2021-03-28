753c8c169c91d08fc80c5a81dfe0abe2ab6865f9
Kristen Picard <kristenpicard93@gmail.com>
2021-03-28 14:55:52 -0400

    adding prompts for each employee type

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
 
-// Uses inquirer to prompt the user with quetions
-// Grabbed this from my README generator to get started on prompts
+// Uses inquirer to prompt the user with starting manager ?s
 managerPrompt(() => {
   inquirer
     .prompt([
@@ -43,6 +42,108 @@ managerPrompt(() => {
         validate: function (input) {
           if (input == " ") {
             console.log("Please enter a valid office number");
+          }
+          return true;
+        },
+      },
+    ])
+
+    .then((data) => {
+      // Making a variable that calls the README content
+      const template = managerPrompt(data); // unsure what this hw will call
+
+      // This creates the new README file (arg1: file name,
+      // arg2 data used to "fill" file, arg3 is if error).
+      fs.writeFile("main.html", template, (err) =>
+        err ? console.log(err) : console.log("Success!")
+      );
+    });
+});
+
+// This is the engineer prompt
+engineerPrompt(() => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the Engineer's name: ",
+        name: "name",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid name");
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        message: "Enter the Engineer's email address: ",
+        name: "email",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter valid email");
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        message: "Enter the Engineer's Github username:",
+        name: "github",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid username");
+          }
+          return true;
+        },
+      },
+    ])
+
+    .then((data) => {
+      // Making a variable that calls the README content
+      const template = managerPrompt(data); // unsure what this hw will call
+
+      // This creates the new README file (arg1: file name,
+      // arg2 data used to "fill" file, arg3 is if error).
+      fs.writeFile("main.html", template, (err) =>
+        err ? console.log(err) : console.log("Success!")
+      );
+    });
+});
+
+// This is the intern prompt
+internPrompt(() => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the Intern's name: ",
+        name: "name",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid name");
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        message: "Enter the Intern's email address: ",
+        name: "email",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter valid email");
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        message: "Enter the name of the school the Intern attended:",
+        name: "school",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid school");
           }
           return true;
         },
