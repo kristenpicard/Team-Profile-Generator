4ffff8ce57da71fc9739980eac41f46dfe068161
Kristen Picard <kristenpicard93@gmail.com>
2021-03-30 14:03:58 -0400

    Fixing prompts

--- index.js
+++ index.js
@@ -5,189 +5,236 @@ const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
 
+// let myTeam = [
+//Want to append teammates?];
+
 // Uses inquirer to prompt the user with starting manager ?s
-// WANTED TO MAKE OWN FUNCTION BUT KEPT SAYING NOT DEFINED? managerPrompt(() => {
-inquirer
-  .prompt([
-    {
-      message: "Let's Build Your Team Profile! (Press Enter to begin)",
-      name: "mssg",
-    },
-    {
-      type: "input",
-      message: "Enter the Manager's name: ",
-      name: "name",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter a valid name");
-        }
-        return true;
+function managerPrompt() {
+  inquirer
+    .prompt([
+      {
+        message: "Let's Build Your Team Profile! (Press Enter to begin)",
+        name: "mssg",
       },
-    },
-    {
-      type: "input",
-      message: "Enter the Manager's email address: ",
-      name: "email",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter valid email");
-        }
-        return true;
+      {
+        type: "input",
+        message: "Enter the Manager's name: ",
+        name: "name",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid name");
+            return false;
+          }
+          return true;
+        },
       },
-    },
-    {
-      type: "input",
-      message: "Enter the Manager's Office #:",
-      name: "onumber",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter a valid office number");
-        }
-        return true;
+      {
+        type: "input",
+        message: "Enter the Manager's email address: ",
+        name: "email",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter valid email");
+            return false;
+          }
+          return true;
+        },
       },
-    },
-    {
-      type: "input",
-      message: "Enter the Manager's ID #:",
-      name: "id",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter a valid ID number");
-        }
-        return true;
+      {
+        type: "input",
+        message: "Enter the Manager's Office #:",
+        name: "onumber",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid office number");
+            return false;
+          }
+          return true;
+        },
       },
-    },
-  ])
+      {
+        type: "input",
+        message: "Enter the Manager's ID #:",
+        name: "id",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid ID number");
+            return false;
+          }
+          return true;
+        },
+      },
+    ])
 
-  .then((data) => {
-    const name = data.name;
-    const email = data.email;
-    const onumber = data.onumber;
-    const id = data.id;
+    .then((data) => {
+      const name = data.name;
+      const email = data.email;
+      const onumber = data.onumber;
+      const id = data.id;
 
-    const member = new Manager(name, email, onumber, id);
-    console.log(member);
-  });
-// });
+      const member = new Manager(name, email, onumber, id);
+      console.log(member);
+      addTeammate();
+    });
+}
 
 // CURRENTLY PROMPTING AT SAME TIME AS MANAGER This is the engineer prompt
-// engineerPrompt(() => {
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Enter the Engineer's name: ",
-      name: "name",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter a valid name");
-        }
-        return true;
+function engineerPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the Engineer's name: ",
+        name: "name",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid name");
+            return false;
+          }
+          return true;
+        },
       },
-    },
-    {
-      type: "input",
-      message: "Enter the Engineer's email address: ",
-      name: "email",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter valid email");
-        }
-        return true;
+      {
+        type: "input",
+        message: "Enter the Engineer's email address: ",
+        name: "email",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter valid email");
+            return false;
+          }
+          return true;
+        },
       },
-    },
-    {
-      type: "input",
-      message: "Enter the Engineer's Github username:",
-      name: "github",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter a valid username");
-        }
-        return true;
+      {
+        type: "input",
+        message: "Enter the Engineer's Github username:",
+        name: "github",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid username");
+            return false;
+          }
+          return true;
+        },
       },
-    },
-    {
-      type: "input",
-      message: "Enter the Engineer's ID #:",
-      name: "id",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter a valid ID number");
-        }
-        return true;
+      {
+        type: "input",
+        message: "Enter the Engineer's ID #:",
+        name: "id",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid ID number");
+            return false;
+          }
+          return true;
+        },
       },
-    },
-  ])
+    ])
 
-  .then((data) => {
-    const name = data.name;
-    const email = data.email;
-    const github = data.github;
-    const id = data.id;
+    .then((data) => {
+      const name = data.name;
+      const email = data.email;
+      const github = data.github;
+      const id = data.id;
 
-    const member = new Manager(name, email, github, id);
-    console.log(member);
-  });
-// });
+      const member = new Engineer(name, email, github, id);
+      console.log(member);
+      addTeammate();
+    });
+}
 
 // CURRENTLY PROMPTING AT SAME TIME AS MANAGER This is the intern prompt
-// internPrompt(() => {
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Enter the Intern's name: ",
-      name: "name",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter a valid name");
-        }
-        return true;
+function internPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the Intern's name: ",
+        name: "name",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid name");
+            return false;
+          }
+          return true;
+        },
       },
-    },
-    {
-      type: "input",
-      message: "Enter the Intern's email address: ",
-      name: "email",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter valid email");
-        }
-        return true;
+      {
+        type: "input",
+        message: "Enter the Intern's email address: ",
+        name: "email",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter valid email");
+            return false;
+          }
+          return true;
+        },
       },
-    },
-    {
-      type: "input",
-      message: "Enter the name of the school the Intern attended:",
-      name: "school",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter a valid school");
-        }
-        return true;
+      {
+        type: "input",
+        message: "Enter the name of the school the Intern attended:",
+        name: "school",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid school");
+            return false;
+          }
+          return true;
+        },
       },
-    },
-    {
-      type: "input",
-      message: "Enter the Intern's ID #:",
-      name: "id",
-      validate: function (input) {
-        if (input == " ") {
-          console.log("Please enter a valid ID number");
-        }
-        return true;
+      {
+        type: "input",
+        message: "Enter the Intern's ID #:",
+        name: "id",
+        validate: function (input) {
+          if (input == " ") {
+            console.log("Please enter a valid ID number");
+            return false;
+          }
+          return true;
+        },
       },
-    },
-  ])
+    ])
 
-  .then((data) => {
-    const name = data.name;
-    const email = data.email;
-    const school = data.school;
-    const id = data.id;
+    .then((data) => {
+      const name = data.name;
+      const email = data.email;
+      const school = data.school;
+      const id = data.id;
 
-    const member = new Manager(name, email, school, id);
-    console.log(member);
-  });
-// });
+      const member = new Intern(name, email, school, id);
+      console.log(member);
+      addTeammate();
+    });
+}
+
+function renderTeam() {
+  console.log("it worked");
+}
+
+function addTeammate() {
+  inquirer
+    .prompt([
+      {
+        type: "checkbox",
+        message: "Would you like to add another team member?",
+        name: "choice",
+        choices: ["Engineer", "Intern", "No, team complete"],
+      },
+    ])
+
+    .then(function (data) {
+      if (data.choice == "Engineer") {
+        engineerPrompt();
+      }
+      if (data.choice == "Intern") {
+        internPrompt();
+      } else {
+        renderTeam();
+      }
+    });
+}
+
+managerPrompt();
