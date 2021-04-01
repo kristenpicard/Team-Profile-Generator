ffd74c93d4b60e1ec8d064f310b30b8723854c5a
Kristen Picard <kristenpicard93@gmail.com>
2021-04-01 13:18:34 -0400

    Got first test to pass for employee

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,15 @@
 const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+  it("should create a generic teammate", () => {
+    const name = "Kristen";
+    const email = "kristenpicard93@gmail.com";
+    const id = "50";
+
+    let emp = new Employee(name, email, id);
+
+    expect(emp.name).toEqual(name);
+    expect(emp.email).toEqual(email);
+    expect(emp.id).toEqual(id);
+  });
+});
